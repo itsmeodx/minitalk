16dd1fc4c2fd9485d827015add537d1121bcf926
itsmeodx <oussama15ouaadich@gmail.com>
2024-09-08 10:21:02 +0100

    Add minitalk.h header file

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oouaadic <oouaadic@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 01:43:22 by oouaadic          #+#    #+#             */
+/*   Updated: 2024/05/09 01:55:41 by oouaadic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif //MINITALK_H
